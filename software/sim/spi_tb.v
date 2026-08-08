c7052ca0978e55730b0a2c2d9829ee1fa247f827
Fran Fodor <49796718+franFodor@users.noreply.github.com>
2026-08-08 12:21:22 +0200

    test(sim): add spi tx/rx testbench

--- software/sim/spi_tb.v
+++ software/sim/spi_tb.v
@@ -6,6 +6,10 @@ module spi_tb;
 
   reg  [31:0] cycle_count;
 
+  reg  [ 7:0] byte_to_receive;
+  reg  [ 7:0] mosi_accum;
+  reg  [ 7:0] bit_count;
+
   spi #(
       .MAX_HALF_PERIOD_COUNT(4)
   ) my_dut (
@@ -32,8 +36,10 @@ module spi_tb;
     cycle_count = 0;
     @(posedge clk);
     reset = 0;
+    @(posedge clk);
     start_signal = 1;
     @(posedge clk);
+    #1;
     start_signal = 0;
     while (!done_signal) begin
       @(posedge clk);
@@ -46,6 +52,42 @@ module spi_tb;
     end else begin
       $display("FAIL");
     end
+  end
+
+  initial begin
+    byte_to_send = 8'b11001010;
+    byte_to_receive = 8'b10010101;
+    bit_count = 0;
+    mosi_accum = 0;
+    miso = byte_to_receive[7];
+    @(posedge start_signal);
+
+    while (!done_signal) begin
+      @(posedge sclk or posedge done_signal);
+      #1;
+      if (!done_signal) begin
+        mosi_accum = {mosi_accum[6:0], mosi};
+        bit_count  = bit_count + 1;
+        @(negedge sclk or posedge done_signal);
+        #1;
+        if (!done_signal) begin
+          miso = byte_to_receive[7-bit_count];
+        end
+      end
+    end
+
+    if (byte_to_send == mosi_accum) begin
+      $display("PASS TX");
+    end else begin
+      $display("FAIL");
+    end
+
+    if (byte_received == byte_to_receive) begin
+      $display("PASS RX");
+    end else begin
+      $display("FAIL");
+    end
     $finish;
   end
+
 endmodule
